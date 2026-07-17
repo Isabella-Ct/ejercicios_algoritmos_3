c5943c64fb0f24785a41c150451baa552aba344a
= <=>
2026-07-17 17:31:41 -0500

    Primeros cambios del repositorio ciclos

--- ciclos_for/ejercicios_for_1.js
+++ ciclos_for/ejercicios_for_1.js
@@ -1,6 +1,5 @@
 //Conteo estándar: Crea un bucle que imprima en la consola los números del 1 al 100 de forma consecutiva.
 
-for(let i=1;i<=100;i=i+1) 
-{
+for(let i=1;i<=100;i++) {
     console.log(i);
 }
