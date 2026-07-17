176d6f6629c2d391645608c9b6aa9f8447a72bf0
= <=>
2026-07-17 17:08:22 -0500

    Subo 5 ejercicios de FOR a mi repositorio

--- ciclos_for/ejercicios_for_5.js
+++ ciclos_for/ejercicios_for_5.js
@@ -1,1 +1,9 @@
 //Acumulación de valores: Crea un programa que sume todos los números enteros del 1 al 5 y muestre el resultado total al finalizar el ciclo.
+
+let numeros = [1, 2, 3, 4, 5];
+let suma = 0;
+
+for (let i = 0;i < numeros.length; i++) {
+    suma = numeros[i]+suma;
+}
+console.log ("Resultado de la suma: " + suma);
