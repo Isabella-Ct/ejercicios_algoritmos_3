3dbac2300a2b3b803f25206174fa0fadc202cfb9
= <=>
2026-07-23 13:52:33 -0500

    Realice el ejercicios 6 y 7

--- ciclos_for/ejercicios_for_6.js
+++ ciclos_for/ejercicios_for_6.js
@@ -1,1 +1,7 @@
 //Recorrido de arreglos: Define un arreglo de nombres y utiliza un bucle para mostrar cada nombre individualmente en la pantalla.
+
+let nombres = ["Isabella", "Margarita", "Patricia", "Samuel"];
+
+for (let i = 0; i < nombres.length; i++) {
+    console.log (nombres[i]);
+}
