c346019cbf3e1e6ebe8f0f2cac816569beae6fc4
= <=>
2026-07-23 22:40:42 -0500

    Terminada la carpeta ciclos_for

--- ciclos_for/ejercicios_for_10.js
+++ ciclos_for/ejercicios_for_10.js
@@ -1,1 +1,12 @@
 //Búsqueda de índice: Dado un arreglo de elementos, utiliza un ciclo para encontrar en qué posición (índice) se encuentra un valor específico y detén la búsqueda una vez hallado.
+
+let elementos = ["sandia", "melon", "pera", "manzana", "banano", "piña"];
+
+for(let i = 0; i<elementos.length; i++){
+    console.log("Posicion " +i+ " es: "+elementos[i]);
+    if(elementos[i]==="banano"){
+        console.log("banano encontrado");
+        break;
+    }
+
+}
